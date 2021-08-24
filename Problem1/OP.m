a225610918a3de56070db6fd63687fab18448ee0
José Lucas <jose.holanda@ee.ufcg.edu.br>
2021-08-24 13:01:02 -0300

    Finalizando a questão e conferindo resultados

--- Problem1/OP.m
+++ Problem1/OP.m
@@ -9,7 +9,21 @@ clc;
 
 %% Main code
 
+% Instanciando as duas variáveis em formato de matriz
+% Matriz M é a tabela 2
+% Vetor F é a função que retrata o investimento de cada instituição
+
 [M,F] = instance();
 
-[F]   = calcF(M);
+% Cálculo de investimento de cada instituição
 
+[F]   = calcF(M)
+
+% Cálculo da função objetivo
+
+[O] = objetivo(F)
+
+% Para que a função objetivo retorne a mesma ordem de grandeza 
+% da função F, calcula-se a raiz de O
+
+o = sqrt(O)
